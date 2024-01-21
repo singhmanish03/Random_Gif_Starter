0ad6eaa83fd43cc902f69f0926597f4a2a4c4bd7
singhmanish03 <mailsinghmanish03@gmail.com>
2024-01-22 00:53:00 +0530

    Added Random GIF

--- src/components/Random.js
+++ src/components/Random.js
@@ -17,7 +17,7 @@ const Random = () => {
     <div className='w-1/2  bg-green-500 rounded-lg border border-black
     flex flex-col items-center gap-y-5 mt-[15px]'>
 
-      <h1 className='mt-[15px] text-2xl underline uppercase font-bold'> A Random Gif</h1>
+      <h1 className='mt-[15px] text-2xl underline uppercase  font-extrabold'> A Random Gif</h1>
 
     {
         loading ? (<Spinner/>) : (<img src= {gif} width="450" />)
